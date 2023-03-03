71a84176235d7106ce2e21b3ed3e7e092fd59601
AkbarFelda <akbarfelda65@gmail.com>
2023-03-03 20:20:59 +0700

    style : [AF] add textview

---
+++ app/src/main/java/com/example/layout/Akbar_Profile.java
@@ -0,0 +1,16 @@
+package com.example.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Akbar_Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_akbar_profile);
+
+
+    }
+}
